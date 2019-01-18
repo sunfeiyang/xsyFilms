40736b0f70a39c3529dd05d1923fde2fd812c27f
sunfeiyang <sfy5941@163.com>
2019-01-18 10:53:20 +0800

    comments and reviews

--- src/app/pages/reviews/reviews.component.ts
+++ src/app/pages/reviews/reviews.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-reviews',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Result;
+
+  url = '/yy/getSubject/reviews/';
+
+  constructor(private http: HttpClient,
+              private route: ActivatedRoute) { }
+
+  // 获取路由传入的参数
+  id = this.route.snapshot.paramMap.get('id');
 
   ngOnInit() {
+    this.http.get(this.url + this.id).subscribe(res => {
+      this.reviews = <Result>res;
+    });
   }
 
 }
+
+export class Result {
+  code: number;
+  msg: String;
+  data: JSON;
+}
+
